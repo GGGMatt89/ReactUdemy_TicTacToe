1771296fd75f161e02f1701237f3b2f1d09f41eb
GGGMatt89 <matt.fntn89@gmail.com>
2024-12-06 15:18:56 +0100

    Lesson 82 completed

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleClick(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const board = [...prevGameBoard.map((innerArray) => [...innerArray])];
+      board[rowIndex][colIndex] = activePlayerSymbol;
+      return board;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function updatePlayerName(e) {
+    setPlayerName(e.target.value);
+  }
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={updatePlayerName}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
